7a770114cfde7f89627414b4f9486f4ba5a31ab0
jenasuraj <jenasuraj742@gmail.com>
2023-06-16 16:25:27 +0530

    done

---
+++ BUTTON-COLOR/index.js
@@ -0,0 +1,39 @@
+let rourkela=document.getElementById('suraj')
+let rajgangpur =document.getElementById('kumar')
+let sundargarh=document.getElementById('jena')
+function red()
+ {
+ 
+    let colorone=document.getElementById('heading')
+    if(colorone.style.color != 'red')
+    {
+        colorone.style.color='red'
+    }
+    else{
+        colorone.style.color='white';
+    }
+ }
+
+ function blue()
+ {
+ 
+    let colortwo=document.getElementById('heading')
+    if(colortwo.style.color != 'blue')
+    {
+        colortwo.style.color='blue'
+    }
+    else{
+        colortwo.style.color='white';
+    }
+ }
+ function green()
+ {
+    let colorthree=document.getElementById('heading');
+    if(colorthree.style.color != 'green')
+    {
+        colorthree.style.color='green';
+    }
+    else{
+        colorthree.style.color='white';
+    }
+ }
